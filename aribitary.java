45040b6d384813223028801a96eb5646ea7264b8
Dropkick <edwardtian619@gmail.com>
2026-09-11 15:46:34 -0400

    Update aribitary.java

--- aribitary.java
+++ aribitary.java
@@ -2,6 +2,7 @@
 class Arbitary {
     public void hello() {
         System.out.println("Hello from Part 5");
+        System.out.println("Hello from Part 9");
     }
     public static void main() {
         hello();
